299427cdf56c666b90be0111ec2ca1cf7083b2c2
Erick Garcia <garcia.erick.cs@gmail.com>
2018-07-16 16:03:38 -0600

    Added a go connection handler to handle concurrent requests

--- client.go
+++ client.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"net"
-	"fmt"
-	"bufio"
-	"os"
-	"strings"
-	"os/signal"
-	"syscall"
+  "net"
+  "fmt"
+  "bufio"
+  "os"
+  "strings"
+  "os/signal"
+  "syscall"
 )
 
 type client struct {
@@ -18,13 +18,13 @@ type client struct {
 
 func main() {
 
-	// connect to this socket
-	conn, _ := net.Dial("tcp", "127.0.0.1:8083")
-  SetupCloseHandler(conn)
+  // connect to this socket
+  conn, _ := net.Dial("tcp", "127.0.0.1:8085")
+  SetupCloseHandlerClient(conn)
 
   generateUuid()
 
-	fmt.Println("Starting Client")
+  fmt.Println("Starting Client")
 
   // read in input from stdin
   reader := bufio.NewReader(os.Stdin)
@@ -33,39 +33,42 @@ func main() {
   name, _ := reader.ReadString('\n')
   fmt.Fprintf(conn, name + "\n")
 
-	for { 
+  for { 
 
-		fmt.Print("Text to send: ")
-		text, _ := reader.ReadString('\n')
+    fmt.Print("Text to send: ")
+    text, _ := reader.ReadString('\n')
 
 
-		// send to socket
-		fmt.Fprintf(conn, text + "\n")
+    // send to socket
+    fmt.Fprintf(conn, text + "\n")
 
-		// listen for reply
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Message from server: "+message)
+    // listen for reply
+    message, _ := bufio.NewReader(conn).ReadString('\n')
+    fmt.Print("Message from server: "+message)
 
     if strings.TrimRight(text, "\n") == "exit" {
       fmt.Println("Closing client")
       break
     }
-	}
+    if text  == "exit" {
+      fmt.Println("Closing client")
+      break
+    }
+  }
 
   conn.Close()
-	os.Exit(1)
+  os.Exit(1)
 }
 
-func SetupCloseHandler(conn net.Conn) {
-	c := make(chan os.Signal, 2)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
+func SetupCloseHandlerClient(conn net.Conn) {
+  c := make(chan os.Signal, 2)
+  signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+  go func() {
+    <-c
 
     //Sending close connection string to server
     fmt.Fprintf(conn, "exit" + "\n")
-
-		fmt.Println("\r- Ctrl+C pressed in Terminal, closing connection")
-		os.Exit(0)
-	}()
+    fmt.Println("\r- Ctrl+C pressed in Terminal, closing connection")
+    os.Exit(0)
+  }()
 }
--- server.go
+++ server.go
@@ -12,56 +12,63 @@ import (
 
 
 func main() {
-  SetupCloseHandler()
 
   fmt.Println("Launching server...")
 
+  // listen on all interfaces
+  ln, _ := net.Listen("tcp", ":8085")
 
+  SetupCloseHandlerServer(ln)
   for {
-    // listen on all interfaces
-    ln, _ := net.Listen("tcp", ":8083")
 
     fmt.Println("Ready to receive")
 
     // run loop forever (or until ctrl-c)
     conn, err := ln.Accept()
 
-    name, _ := bufio.NewReader(conn).ReadString('\n')
-    name = strings.TrimRight(name, "\n")
-    fmt.Println(name + " has logged in")
-
-    for {
-
-      if err != nil {
-        fmt.Println("An error has ocurred, closing server")
-        os.Exit(2)
-      }
-
-      // will listen for message to process ending in newline (\n)
-      message, _ := bufio.NewReader(conn).ReadString('\n')
-
-
-      fmt.Print("Message Received:", string(message))
-      newmessage := strings.ToUpper(message)
-      conn.Write([]byte(newmessage + "\n"))
-
-      if strings.TrimRight(message, "\n") == "exit" {
-        break
-      }
+    if err != nil {
+      fmt.Println("An error has ocurred, closing server")
+      os.Exit(2)
     }
 
+
+    go handleConnection(conn)
+
+
     fmt.Println("Closing Connection")
-    ln.Close()
 
   }
 }
 
-func SetupCloseHandler() {
+func handleConnection(conn net.Conn){
+  name, _ := bufio.NewReader(conn).ReadString('\n')
+  name = strings.TrimRight(name, "\n")
+  fmt.Println(name + " has logged in")
+  for {
+    // will listen for message to process ending in newline (\n)
+    message, _ := bufio.NewReader(conn).ReadString('\n')
+
+
+    fmt.Print("Message Received:", string(message))
+    newmessage := strings.ToUpper(message)
+    conn.Write([]byte(newmessage + "\n"))
+
+    if strings.TrimRight(message, "\n") == "exit" {
+      conn.Close()
+      return
+    }
+  }
+
+}
+
+func SetupCloseHandlerServer(ln net.Listener) {
   c := make(chan os.Signal, 2)
   signal.Notify(c, os.Interrupt, syscall.SIGTERM)
   go func() {
     <-c
     fmt.Println("\r- Ctrl+C pressed in Terminal")
+    fmt.Println("\r- Closing listener and exiting program")
+    ln.Close()
     os.Exit(0)
   }()
 }
